3d93fdcc4df2b73d1ba507e99329040671bbac47
Oliver Cox <olivercjcox@hotmail.co.uk>
2019-08-28 13:08:53 +0100

    Made TQDM optional, added comments

--- scrape.py
+++ scrape.py
@@ -1,15 +1,27 @@
 import urllib.request
 import json
-from tqdm import tqdm
 import time
 
+# Check if tqdm is installed; if it is, use it, if it isn't, define a function that just returns its input so that we don't crash later in the program.
+try:
+    from tqdm import tqdm
+except:
+    print("tqdm is not present")
+    def tqdm(stuff):
+        return stuff
+else:
+    print("tqdm is present.")
+
+# Delay in seconds between each request.
 delay = 0.2
+
 errors = "Errors:"
 
 f = open("data.json", "r")
 data = json.loads(f.read())
 f.close()
 
+# For each key-value pair from the JSON file, attempt to download it, falling back on smaller sizes if it fails.
 for url, name in tqdm(data.items()):
     try:
         urllib.request.urlretrieve(url, "images/" + name.replace("&", "AND").replace("/", "SLASH") + ".png")
